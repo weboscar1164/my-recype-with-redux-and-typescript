0d827fe91b340d374a5b92211e61c71796796a31
webshoposcar1164 <cheaptrip1164@gmail.com>
2024-05-06 21:26:28 +0900

    Serchbarスタイリング

--- src/components/header/Searchbar.tsx
+++ src/components/header/Searchbar.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Searchbar.scss";
 
 const Searchbar = () => {
-	return <div>Searchbar</div>;
+	return (
+		<div className="searchbar">
+			<input type="text" placeholder="レシピを検索" />
+			<SearchIcon className="searchbarIcon" />
+		</div>
+	);
 };
 
 export default Searchbar;
