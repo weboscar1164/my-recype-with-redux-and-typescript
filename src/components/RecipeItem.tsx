439614890480c8fc41b0cfd6116638c971e1dd51
webshoposcar1164 <cheaptrip1164@gmail.com>
2024-09-20 16:45:21 +0900

    リファクタリング：メインのリストを コンポーネント化

---
+++ src/components/RecipeItem.tsx
@@ -0,0 +1,153 @@
+import { useState } from "react";
+import { FavoriteState, InitialRecipeState, RecipeListItem } from "../Types";
+import RecipeImage from "./contents/RecipeImage";
+import { setRecipeInfo } from "../features/recipeSlice";
+import { useNavigate } from "react-router-dom";
+import {
+	useAddFavorite,
+	useAppDispatch,
+	useAppSelector,
+	useDeleteFavorite,
+} from "../app/hooks/hooks";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+const RecipeItem = ({
+	currentRecipes,
+	currentPage,
+}: {
+	currentRecipes: RecipeListItem[];
+	currentPage: number;
+}) => {
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
+	const [recipeList, setRecipeList] = useState(currentRecipes);
+
+	const [animatingFavIcon, setAnimatingFavIcon] = useState<string | null>(null);
+
+	const favorites = useAppSelector((state) => state.favorites);
+	const user = useAppSelector((state) => state.user.user);
+
+	const { addFavoriteAsync } = useAddFavorite();
+	const { deleteFavoriteAsync } = useDeleteFavorite();
+
+	const handleClickRecipe = async (recipeId: string) => {
+		const docRef = doc(db, "recipes", recipeId);
+		const docSnap = await getDoc(docRef);
+
+		if (docSnap.exists()) {
+			const currentRecipe = docSnap.data();
+			const newRecipe: InitialRecipeState = {
+				isPublic: currentRecipe.isPublic,
+				recipeName: currentRecipe.recipeName,
+				recipeImageUrl: currentRecipe.recipeImageUrl,
+				comment: currentRecipe.comment,
+				serves: currentRecipe.serves,
+				materials: currentRecipe.materials,
+				procedures: currentRecipe.procedures,
+				recipeId: recipeId,
+				user: currentRecipe.user,
+				userDisprayName: currentRecipe.userDisprayName,
+				favoriteCount: currentRecipe.favoriteCount,
+			};
+
+			console.log("newRecipe: ", newRecipe);
+			// console.log(newRecipe);
+			dispatch(setRecipeInfo(newRecipe));
+			navigate(`/recipe?currentPage=${currentPage}`);
+		} else {
+			console.log("no sutch document!");
+		}
+	};
+
+	// お気に入り切り替え
+	const handleClickFavorite = async (
+		userId: string | undefined,
+		recipeId: string,
+		recipeName: string
+	) => {
+		if (userId) {
+			const isFavorite = favorites.some(
+				(favorite: FavoriteState) => favorite.recipeId === recipeId
+			);
+			if (recipeId && isFavorite) {
+				await deleteFavoriteAsync(userId, recipeId);
+				updateFavoriteCount(recipeId, -1);
+			} else {
+				await addFavoriteAsync(userId, recipeId, recipeName);
+				updateFavoriteCount(recipeId, 1);
+			}
+			setAnimatingFavIcon(recipeId);
+
+			setTimeout(() => setAnimatingFavIcon(null), 300);
+		} else {
+			alert("お気に入り機能を使用するにはログインしてください。");
+		}
+	};
+
+	// お気に入りカウンターの操作
+	const updateFavoriteCount = (recipeId: string, increment: number) => {
+		setRecipeList((prevList) =>
+			prevList.map((recipe) =>
+				recipe.recipeId === recipeId
+					? { ...recipe, favoriteCount: recipe.favoriteCount + increment }
+					: recipe
+			)
+		);
+	};
+	return (
+		<ul>
+			{recipeList.map((item: RecipeListItem) => (
+				<li key={item.recipeId}>
+					<div
+						className="recipeListItemLeft"
+						onClick={() => handleClickRecipe(item.recipeId)}
+					>
+						<div className="recipeListImg">
+							<RecipeImage
+								src={item.recipeImageUrl ? item.recipeImageUrl : "noimage.jpg"}
+								alt={item.recipeName}
+							/>
+						</div>
+						<h3>{item.recipeName}</h3>
+					</div>
+					{item.isPublic == 1 ? (
+						<div
+							className="recipeListFav"
+							onClick={() =>
+								handleClickFavorite(user?.uid, item.recipeId, item.recipeName)
+							}
+						>
+							{item.recipeId &&
+							favorites.some(
+								(favorite: FavoriteState) => favorite.recipeId === item.recipeId
+							) ? (
+								<FavoriteIcon
+									className={`recipeHeaderFavIcon ${
+										animatingFavIcon === item.recipeId &&
+										"recipeHeaderFavIconAnimation"
+									}`}
+								/>
+							) : (
+								<FavoriteBorderIcon
+									className={`recipeHeaderFavIcon ${
+										animatingFavIcon === item.recipeId &&
+										"recipeHeaderFavIconAnimation"
+									}`}
+								/>
+							)}
+							<span className="recipeHeaderFavCount">{item.favoriteCount}</span>
+						</div>
+					) : (
+						<div className="recipeHeaderPrivate">非公開</div>
+					)}
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default RecipeItem;
--- src/components/contents/RecipeList.tsx
+++ src/components/contents/RecipeList.tsx
@@ -1,30 +1,19 @@
 import { useEffect, useState } from "react";
 import "./RecipeList.scss";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase";
 import {
-	useAppDispatch,
 	useAppSelector,
 	useGetRecipeList,
-	useAddFavorite,
-	useDeleteFavorite,
 	useFetchAdminsAndIgnores,
 } from "../../app/hooks/hooks";
-import { FavoriteState, InitialRecipeState, RecipeListItem } from "../../Types";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { setRecipeInfo } from "../../features/recipeSlice";
-import RecipeImage from "./RecipeImage";
+import { FavoriteState, RecipeListItem } from "../../Types";
+import { useSearchParams } from "react-router-dom";
 import Pagination from "../Pagination";
+import RecipeItem from "../RecipeItem";
 
 const recipeList = ({ listMode }: { listMode: string }) => {
-	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
 	const [searchParams, setSearchParams] = useSearchParams();
 
 	const [recipeList, setRecipeList] = useState<RecipeListItem[]>([]);
-	const [animatingFavIcon, setAnimatingFavIcon] = useState<string | null>(null);
 	const [ignoreIdList, setIgnoreIdList] = useState<string[]>([]);
 
 	const initialPage = Number(searchParams.get("page")) || 1;
@@ -35,8 +24,6 @@ const recipeList = ({ listMode }: { listMode: string }) => {
 	const user = useAppSelector((state) => state.user.user);
 	const searchWord = useAppSelector((state) => state.searchWord);
 
-	const { addFavoriteAsync } = useAddFavorite();
-	const { deleteFavoriteAsync } = useDeleteFavorite();
 	const { getRecipeList } = useGetRecipeList();
 	const { fetchAdminsAndIgnores } = useFetchAdminsAndIgnores();
 
@@ -63,72 +50,6 @@ const recipeList = ({ listMode }: { listMode: string }) => {
 			});
 		}
 	}, [currentPage, setSearchParams, searchParams]);
-
-	// recipeクリック時詳細ページにジャンプ
-	const handleClickRecipe = async (recipeId: string) => {
-		const docRef = doc(db, "recipes", recipeId);
-		const docSnap = await getDoc(docRef);
-
-		if (docSnap.exists()) {
-			const currentRecipe = docSnap.data();
-			const newRecipe: InitialRecipeState = {
-				isPublic: currentRecipe.isPublic,
-				recipeName: currentRecipe.recipeName,
-				recipeImageUrl: currentRecipe.recipeImageUrl,
-				comment: currentRecipe.comment,
-				serves: currentRecipe.serves,
-				materials: currentRecipe.materials,
-				procedures: currentRecipe.procedures,
-				recipeId: recipeId,
-				user: currentRecipe.user,
-				userDisprayName: currentRecipe.userDisprayName,
-				favoriteCount: currentRecipe.favoriteCount,
-			};
-
-			console.log("newRecipe: ", newRecipe);
-			// console.log(newRecipe);
-			dispatch(setRecipeInfo(newRecipe));
-			navigate(`/recipe?currentPage=${currentPage}`);
-		} else {
-			console.log("no sutch document!");
-		}
-	};
-
-	// お気に入り切り替え
-	const handleClickFavorite = async (
-		userId: string | undefined,
-		recipeId: string,
-		recipeName: string
-	) => {
-		if (userId) {
-			const isFavorite = favorites.some(
-				(favorite: FavoriteState) => favorite.recipeId === recipeId
-			);
-			if (recipeId && isFavorite) {
-				await deleteFavoriteAsync(userId, recipeId);
-				updateFavoriteCount(recipeId, -1);
-			} else {
-				await addFavoriteAsync(userId, recipeId, recipeName);
-				updateFavoriteCount(recipeId, 1);
-			}
-			setAnimatingFavIcon(recipeId);
-
-			setTimeout(() => setAnimatingFavIcon(null), 300);
-		} else {
-			alert("お気に入り機能を使用するにはログインしてください。");
-		}
-	};
-
-	// お気に入りカウンターの操作
-	const updateFavoriteCount = (recipeId: string, increment: number) => {
-		setRecipeList((prevList) =>
-			prevList.map((recipe) =>
-				recipe.recipeId === recipeId
-					? { ...recipe, favoriteCount: recipe.favoriteCount + increment }
-					: recipe
-			)
-		);
-	};
 
 	// 表示するリストのソート
 	const sortedRecipes = recipeList.filter((recipe) => {
@@ -170,6 +91,20 @@ const recipeList = ({ listMode }: { listMode: string }) => {
 
 	const totalPages = Math.ceil(sortedRecipes.length / recipesPerPage);
 
+	//ページネーションのためのインデックス計算
+	const indexOfLastRecipe = currentPage * recipesPerPage;
+	const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+	const currentRecipes = sortedRecipes.slice(
+		indexOfFirstRecipe,
+		indexOfLastRecipe
+	);
+
+	console.log(currentRecipes);
+
+	const handlePageChange = (pageNumber: number) => {
+		setCurrentPage(pageNumber);
+	};
+
 	const handleRenderTitle = (listMode: string) => {
 		switch (listMode) {
 			case "favorites":
@@ -180,118 +115,6 @@ const recipeList = ({ listMode }: { listMode: string }) => {
 				return "レシピ一覧";
 		}
 	};
-
-	//ページネーションのためのインデックス計算
-	const indexOfLastRecipe = currentPage * recipesPerPage;
-	const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
-	const currentRecipes = sortedRecipes.slice(
-		indexOfFirstRecipe,
-		indexOfLastRecipe
-	);
-
-	const handlePageChange = (pageNumber: number) => {
-		setCurrentPage(pageNumber);
-	};
-
-	const handleNextPage = () => {
-		if (currentPage < pageNumbers.length) {
-			setCurrentPage(currentPage + 1);
-		}
-	};
-
-	const handlePrevPage = () => {
-		if (currentPage > 1) {
-			setCurrentPage(currentPage - 1);
-		}
-	};
-
-	const pageNumbers: number[] = [];
-	for (let i = 1; i <= Math.ceil(sortedRecipes.length / recipesPerPage); i++) {
-		pageNumbers.push(i);
-	}
-
-	const maxPageNumbersToshow = 5;
-	const totalPageNumbers = pageNumbers.length;
-
-	const renderPageNumbers = () => {
-		if (totalPageNumbers <= maxPageNumbersToshow) {
-			return pageNumbers.map((number) => (
-				<button
-					key={number}
-					onClick={() => handlePageChange(number)}
-					className={`paginationButton ${
-						number === currentPage ? "paginationActive" : "paginationInactive"
-					}`}
-				>
-					{number}
-				</button>
-			));
-		}
-
-		const pages = [];
-		let startPage, endPage;
-
-		if (currentPage <= 3) {
-			startPage = 1;
-			endPage = maxPageNumbersToshow;
-		} else if (currentPage + 2 >= totalPageNumbers) {
-			startPage = totalPageNumbers - 4;
-			endPage = totalPageNumbers;
-		} else {
-			startPage = currentPage - 2;
-			endPage = currentPage + 2;
-		}
-
-		// Add first page and ellipsis if needed
-		if (startPage > 1) {
-			pages.push(
-				<button
-					key={1}
-					onClick={() => handlePageChange(1)}
-					className="paginationButton paginationInactive"
-				>
-					1
-				</button>
-			);
-			if (startPage > 2) {
-				pages.push(<span key="start-ellipsis">...</span>);
-			}
-		}
-
-		// Add page numbers
-		for (let i = startPage; i <= endPage; i++) {
-			pages.push(
-				<button
-					key={i}
-					onClick={() => handlePageChange(i)}
-					className={`paginationButton ${
-						i === currentPage ? "paginationActive" : "paginationInactive"
-					}`}
-				>
-					{i}
-				</button>
-			);
-		}
-
-		// Add last page and ellipsis if needed
-		if (endPage < totalPageNumbers) {
-			if (endPage < totalPageNumbers - 1) {
-				pages.push(<span key="end-ellipsis">...</span>);
-			}
-			pages.push(
-				<button
-					key={totalPageNumbers}
-					onClick={() => handlePageChange(totalPageNumbers)}
-					className="paginationButton paginationInactive"
-				>
-					{totalPageNumbers}
-				</button>
-			);
-		}
-
-		return pages;
-	};
-
 	return (
 		<div className="recipeList">
 			<div className="recipeListContainer">
@@ -299,65 +122,10 @@ const recipeList = ({ listMode }: { listMode: string }) => {
 				<h3>{searchWord && `検索結果: ${searchWord}`}</h3>
 				{currentRecipes.length !== 0 ? (
 					<>
-						<ul>
-							{currentRecipes.map((item: RecipeListItem) => (
-								<li key={item.recipeId}>
-									<div
-										className="recipeListItemLeft"
-										onClick={() => handleClickRecipe(item.recipeId)}
-									>
-										<div className="recipeListImg">
-											<RecipeImage
-												src={
-													item.recipeImageUrl
-														? item.recipeImageUrl
-														: "noimage.jpg"
-												}
-												alt={item.recipeName}
-											/>
-										</div>
-										<h3>{item.recipeName}</h3>
-									</div>
-									{item.isPublic == 1 ? (
-										<div
-											className="recipeListFav"
-											onClick={() =>
-												handleClickFavorite(
-													user?.uid,
-													item.recipeId,
-													item.recipeName
-												)
-											}
-										>
-											{item.recipeId &&
-											favorites.some(
-												(favorite: FavoriteState) =>
-													favorite.recipeId === item.recipeId
-											) ? (
-												<FavoriteIcon
-													className={`recipeHeaderFavIcon ${
-														animatingFavIcon === item.recipeId &&
-														"recipeHeaderFavIconAnimation"
-													}`}
-												/>
-											) : (
-												<FavoriteBorderIcon
-													className={`recipeHeaderFavIcon ${
-														animatingFavIcon === item.recipeId &&
-														"recipeHeaderFavIconAnimation"
-													}`}
-												/>
-											)}
-											<span className="recipeHeaderFavCount">
-												{item.favoriteCount}
-											</span>
-										</div>
-									) : (
-										<div className="recipeHeaderPrivate">非公開</div>
-									)}
-								</li>
-							))}
-						</ul>
+						<RecipeItem
+							currentRecipes={currentRecipes}
+							currentPage={currentPage}
+						/>
 						<Pagination
 							currentPage={currentPage}
 							totalPages={totalPages}
